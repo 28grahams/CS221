0a33dd5033577390e3c687fd5949a38f78ef185b
Theo Graham <theograham@me.com>
2019-10-11 19:20:17 -0700

    Started project, added main and isStrongPassword function

---
+++ PasswordStrengthMeter.c
@@ -0,0 +1,32 @@
+//
+// Created by Theodore Graham on 10/11/19.
+//
+// PASSWORD STRENGTH METER
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool isStrongPassword(const char* username, const char* password){
+    // if (characters =< 8)
+
+    // if (1 uppercase)
+
+    // if (1 lowercase)
+
+    // if (1 number)
+
+    // if (string =< 4) lowercase/uppercase/numbers
+
+    // if (doesn't contain username)
+}
+
+int main(void){
+    char[] username = {""};
+    char[] password = {""};
+    printf("Enter a username\n");
+    scanf("%s", &username);
+    printf("Enter a password\n");
+    scanf("%s", &password);
+}
+
