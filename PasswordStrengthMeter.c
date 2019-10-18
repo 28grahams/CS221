efb2a4347f31c08cffedaea147359f5030d0542c
Theo Graham <theograham@me.com>
2019-10-17 19:47:40 -0700

    Finished Validation for at least 4 uppercase/lowercase

--- PasswordStrengthMeter.c
+++ PasswordStrengthMeter.c
@@ -5,20 +5,19 @@
 
 #include <stdio.h>
 #include <string.h>
+//#include <stdbool.h>
+// can't get stdbool.h to work
 
 typedef int bool;
 #define false 0
 #define true 1
 
-int characters, uCase=0, lCase=0, numb=0;
-
 bool isStrongPassword(const char* username, const char* password){
+    int uCase=0, lCase=0, numb=0, consecLC=0, consecUC=0, consecN=0;
     const char *pwPtr = password;
+    const char *pwPtr2 = password;
     unsigned long passwordLen = strlen(password);
-    if (passwordLen <= 8){
-        printf("Password must contain at least eight characters.\n");
-        characters = 1;
-    }
+
     while (*pwPtr != '\0') {
         if (*pwPtr >= 'A' && *pwPtr <= 'Z'){
             uCase++;
@@ -32,6 +31,26 @@ bool isStrongPassword(const char* username, const char* password){
         pwPtr++;
 
     }
+    for (unsigned long i=0; i < passwordLen; i++) {
+        if (*(pwPtr2+i) >= 'A' && *(pwPtr2+i) <= 'Z'){
+            if ((*(pwPtr2+i+1) >= 'A' && *(pwPtr2+i+1) <= 'Z') && (*(pwPtr2+i+2) >= 'A' && *(pwPtr2+i+2) <= 'Z')&& (*(pwPtr2+i+3) >= 'A' && *(pwPtr2+i+3) <= 'Z')){
+                consecUC++;
+            }
+        }
+        if (*(pwPtr2+i) >= 'a' && *(pwPtr2+i) <= 'z'){
+            if ((*(pwPtr2+i+1) >= 'a' && *(pwPtr2+i+1) <= 'z') && (*(pwPtr2+i+2) >= 'a' && *(pwPtr2+i+2) <= 'z')&& (*(pwPtr2+i+3) >= 'a' && *(pwPtr2+i+3) <= 'z')){
+                consecLC++;
+            }
+        }
+        if (*(pwPtr2+i) >= 'A' && *(pwPtr2+i) <= 'z'){
+            if ((*(pwPtr2+i+1) >= 'A' && *(pwPtr2+i+1) <= 'z') && (*(pwPtr2+i+2) >= 'A' && *(pwPtr2+i+2) <= 'z')&& (*(pwPtr2+i+3) >= 'A' && *(pwPtr2+i+3) <= 'z')){
+                consecN++;
+            }
+        }
+    }
+    if (passwordLen <= 8){
+        printf("Password must contain at least eight characters.\n");
+    }
     if (uCase == 0){
         printf("Password must contain at least one uppercase letter.\n");
     }
@@ -41,11 +60,9 @@ bool isStrongPassword(const char* username, const char* password){
     if (numb == 0){
     printf("Password must contain at least one number.\n");
     }
-    if ((characters+uCase+lCase+numb) < 3) {
-        printf("Enter a password fulfilling the requirements.\n");
+    if (((consecLC==0) || (consecUC==0)) && (consecN==0)){
+        printf("Password must have 4 uppercase, lowercase, or numbers in a row");
     }
-    // if (string =< 4) lowercase/uppercase/numbers
-
     // if (doesn't contain username)
     return false;
 }
